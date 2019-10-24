59a5ed5059e6e55024c08c0c7de5a9c17a44d480
100148965 <canterojessie@gmail.com>
2019-10-24 15:27:57 -0500

    started strings poject repository

---
+++ CodesAndStrings.py
@@ -0,0 +1,23 @@
+# strings
+
+# concatenation
+
+firstName = "Jesus"
+lastName = "Monte"
+
+print(firstName + " " + lastName)
+
+name = firstName + " " + lastName
+lastFirst = lastName + ", " + firstName
+
+print(lastFirst)
+
+# repition
+
+print("uh "*2 + "AAAAAAAAA")
+
+def imTiredofSchool():
+    print("me, "*3 + " young Jessie")
+    print(" is exhausted")
+    print("Why must you ask?"*3 "school")
+
