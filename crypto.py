cf69b216ce2fb9ee2e6bf1741e75e1911c4504d0
jessiecantdoanything <54687557+jessiecantdoanything@users.noreply.github.com>
2019-11-03 19:10:18 -0600

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -28,3 +28,37 @@ def scramble2Decrypt(cipherText):
         plaintext = plainText + evenChars[-1]
 
     return plainText
+def letterToIndex(letter):
+    from string import ascii_lowercase
+    alphabet = ascii_lowercase + ' '
+    idx = alphabet.find(letter)
+    if idx == -1:       #means that it wasn't in the alphabet
+        print("error:", letter, "is not in the alphabet")
+    return idx
+
+
+letter = input("wots your mesij")
+key = input("wots youe shift")
+def letterToIndex(letter, key):
+    from string import ascii_lowercase
+    alphabet = ascii_lowercase + ' '
+
+    for i in range(0, len(letter) + 1):
+        idx = alphabet.find(letter) + key
+
+
+        if idx > 26:
+            idx = idx - 26
+
+
+
+    let = ascii_lowercase[idx]
+
+
+    return let
+
+
+
+print(letterToIndex("y", 4))
+
+
